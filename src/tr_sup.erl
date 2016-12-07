2fd7a6958347237fbe578c47ed344353c82ab794
Blaise Fringel <ffringel9@gmail.com>
2016-12-07 22:59:26 +0100

    TCP RPC Supervisor

---
+++ src/tr_sup.erl
@@ -0,0 +1,66 @@
+%%%-------------------------------------------------------------------
+%%% @author Blaise Fringel
+%%% @copyright (C) 2016, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 07. Dec 2016 10:50 PM
+%%%-------------------------------------------------------------------
+-module(tr_sup).
+-author("Blaise Fringel").
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the supervisor
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(start_link() ->
+  {ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
+start_link() ->
+  supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%%===================================================================
+%%% Supervisor callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever a supervisor is started using supervisor:start_link/[2,3],
+%% this function is called by the new process to find out about
+%% restart strategy, maximum restart frequency and child
+%% specifications.
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(init(Args :: term()) ->
+  {ok, {SupFlags :: {RestartStrategy :: supervisor:strategy(),
+    MaxR :: non_neg_integer(), MaxT :: non_neg_integer()},
+    [ChildSpec :: supervisor:child_spec()]
+  }} |
+  ignore |
+  {error, Reason :: term()}).
+init([]) ->
+  Server = {tr_server, {
+    tr_server, start_link, []},
+    permanent, 2000, worker, [tr_server]},
+  {ok, {{one_for_one, 0, 1}, [Server]}}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
